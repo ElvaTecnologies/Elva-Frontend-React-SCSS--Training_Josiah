37b7143409ebc690f626efd464f556f4dfc6af81
Jovick-coder <victorjosiahm3@gmail.com>
2022-08-07 20:21:49 +0100

    hero component and styling

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import heroImage from "../../assets/images/image-mockups.png";
+import "./HeroComponent.scss";
+
+function HeroComponent() {
+  return (
+    <div className="HeroComponent">
+      <div className="text-div">
+        <h1> Next generation digital banking</h1>
+        <p>
+          {" "}
+          Take your financial life online. Your Easybank account will be a
+          one-stop-shop for spending, saving, budgeting, investing, and much
+          more.
+        </p>
+
+        <div className="button-div">
+          <button className="fancy"> Request Invite</button>
+        </div>
+      </div>
+      <div className="image-div">
+        <img src={heroImage} alt="" />
+      </div>
+    </div>
+  );
+}
+
+export default HeroComponent;
