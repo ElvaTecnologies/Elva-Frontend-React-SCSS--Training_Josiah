2dea16a84c905038589f4be5180ec87574972384
Jovick-coder <victorjosiahm3@gmail.com>
2022-08-09 09:29:00 +0100

    responsive nav for mobile view

--- src/components/Header/NavBar.jsx
+++ src/components/Header/NavBar.jsx
@@ -1,13 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import logoImage from "../../assets/images/logo.svg";
+import "./navComponent.scss";
+import { FaBars, FaXbox } from "react-icons/fa";
 
 function NavBarComponent() {
+  const [menuOpen, setMenuOpen] = useState(true);
   return (
     <div className="NavBarComponent">
       <div className="logo">
         <img src={logoImage} alt="logo" />
       </div>
-      <ul>
+      <ul style={menuOpen ? { visibility: "visible" } : null}>
         <li>Home</li>
         <li>About</li>
         <li>Contact</li>
@@ -15,6 +18,16 @@ function NavBarComponent() {
         <li>Careers</li>
       </ul>
       <button className="fancy"> Request Invite</button>
+      <button
+        className="mobile-menu-button"
+        onClick={() => {
+          setMenuOpen(!menuOpen);
+        }}
+      >
+        {menuOpen ? "x" : <FaBars />}
+        {/* <FaBars /> */}
+        {/* <FaXbox /> */}
+      </button>
     </div>
   );
 }
