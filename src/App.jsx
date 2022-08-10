145d23305ce9029547b84d1c398af130b815a1f0
Jovick-coder <victorjosiahm3@gmail.com>
2022-08-10 12:06:52 +0100

    create a new file for object

--- src/App.jsx
+++ src/App.jsx
@@ -2,64 +2,9 @@ import React from "react";
 import "./App.scss";
 import NavBarComponent from "./components/Header/NavBar";
 import HeroComponent from "./components/Hero/Hero";
-import currencyImage from "./assets/images/image-currency.jpg";
-import confettiImage from "./assets/images/image-confetti.jpg";
-import planeImage from "./assets/images/image-plane.jpg";
-import restaurantImage from "./assets/images/image-restaurant.jpg";
-import iconApi from "./assets/images/icon-api.svg";
-import onboardingApi from "./assets/images/icon-onboarding.svg";
-import onlineApi from "./assets/images/icon-online.svg";
-import budgetingApi from "./assets/images/icon-budgeting.svg";
 import FooterComponent from "./components/footer/FooterComponent";
+import { articleObjects, whyChooseUsObject } from "./objects";
 function App() {
-  const whyChooseUsObject = [
-    {
-      img: onlineApi,
-      header: "Online Banking",
-      text: "Our modern web and mobile applications allow you to keep track of your finances wherever you are in the world.",
-    },
-    {
-      img: budgetingApi,
-      header: "Simple Budgeting",
-      text: "See exactly where your money goes each month. Receive notifications when you’re close to hitting your limits.",
-    },
-    {
-      img: onboardingApi,
-      header: "Fast Onboarding",
-      text: "We don’t do branches. Open your account in minutes online and start taking control of your finances right away.",
-    },
-    {
-      img: iconApi,
-      header: "Open API",
-      text: "Manage your savings, investments, pension, and much more from one account. Tracking your money has never been easier.",
-    },
-  ];
-  const articleObjects = [
-    {
-      img: currencyImage,
-      header: "Receive money in any currency with no fees",
-      text: "The world is getting smaller and we’re becoming more mobile. So why should you be forced to only receive money in a single …",
-      author: "Claire Robinson",
-    },
-    {
-      img: restaurantImage,
-      header: "Treat yourself without worrying about money",
-      text: "Our simple budgeting feature allows you to separate out your spending and set realistic limits each month. That means you …",
-      author: "Wilson Hutton",
-    },
-    {
-      img: planeImage,
-      header: "Take your Easybank card wherever you go",
-      text: "We want you to enjoy your travels. This is why we don’t charge any fees on purchases while you’re abroad. We’ll even show you …",
-      author: "Wilson Hutton",
-    },
-    {
-      img: confettiImage,
-      header: "Our invite-only Beta accounts are now live!",
-      text: "After a lot of hard work by the whole team, we’re excited to launch our closed beta It’s easy to request an invite through the site ...",
-      author: "Claire Robinson",
-    },
-  ];
   return (
     <>
       <div className="App">
---
+++ src/objects.js
@@ -0,0 +1,57 @@
+import currencyImage from "./assets/images/image-currency.jpg";
+import confettiImage from "./assets/images/image-confetti.jpg";
+import planeImage from "./assets/images/image-plane.jpg";
+import restaurantImage from "./assets/images/image-restaurant.jpg";
+import iconApi from "./assets/images/icon-api.svg";
+import onboardingApi from "./assets/images/icon-onboarding.svg";
+import onlineApi from "./assets/images/icon-online.svg";
+import budgetingApi from "./assets/images/icon-budgeting.svg";
+
+export const whyChooseUsObject = [
+  {
+    img: onlineApi,
+    header: "Online Banking",
+    text: "Our modern web and mobile applications allow you to keep track of your finances wherever you are in the world.",
+  },
+  {
+    img: budgetingApi,
+    header: "Simple Budgeting",
+    text: "See exactly where your money goes each month. Receive notifications when you’re close to hitting your limits.",
+  },
+  {
+    img: onboardingApi,
+    header: "Fast Onboarding",
+    text: "We don’t do branches. Open your account in minutes online and start taking control of your finances right away.",
+  },
+  {
+    img: iconApi,
+    header: "Open API",
+    text: "Manage your savings, investments, pension, and much more from one account. Tracking your money has never been easier.",
+  },
+];
+export const articleObjects = [
+  {
+    img: currencyImage,
+    header: "Receive money in any currency with no fees",
+    text: "The world is getting smaller and we’re becoming more mobile. So why should you be forced to only receive money in a single …",
+    author: "Claire Robinson",
+  },
+  {
+    img: restaurantImage,
+    header: "Treat yourself without worrying about money",
+    text: "Our simple budgeting feature allows you to separate out your spending and set realistic limits each month. That means you …",
+    author: "Wilson Hutton",
+  },
+  {
+    img: planeImage,
+    header: "Take your Easybank card wherever you go",
+    text: "We want you to enjoy your travels. This is why we don’t charge any fees on purchases while you’re abroad. We’ll even show you …",
+    author: "Wilson Hutton",
+  },
+  {
+    img: confettiImage,
+    header: "Our invite-only Beta accounts are now live!",
+    text: "After a lot of hard work by the whole team, we’re excited to launch our closed beta It’s easy to request an invite through the site ...",
+    author: "Claire Robinson",
+  },
+];
